8c5fd1467e48ffd49a0416784f62d55959061c23
Vahan199 <Vahanxxx1@icloud.com>
2022-05-14 16:23:24 +0400

    test1

--- SM.py
+++ SM.py
@@ -1,10 +1,4 @@
 # print('Hello Albert')
 # print('Hello Smcode')
 
-
-xndir 
-x = [-1, 4, 5, 8, -10, 20]
-y = int(input('введите число '))
-for i in x:
-    if i == y:
-        print(x.index(y))
+# https://github.com/Vahan199/PythonSmcode.git
