6892e2e9b3fc20527ff03b5a04a8fe68836e0d0e
Vahan199 <Vahanxxx1@icloud.com>
2022-05-14 19:05:18 +0400

    xndir3

--- Sm2.py
+++ Sm2.py
@@ -1,6 +1,18 @@
 # 1.1
 # x = [-1, 4, 5, 8, -10, 20]
-# y = int(input('введите число '))
+# y =int(input('введите число '))
 # for i in x:
 #     if i == y:
 #         print(x.index(y))
+    
+
+x = [-1, 4, 5, 8, -10, 20]
+y =int(input('введите число '))
+z =[]
+for i in x:
+    if abs(i - y) == 0:
+        print(x.index(i))
+    elif abs(i - y) > abs((i + 1) - y):
+        c = i
+print(c)
+        
