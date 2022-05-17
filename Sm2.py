296e59b613718f9389c2db51e97664869c97ddd3
Vahan199 <Vahanxxx1@icloud.com>
2022-05-17 21:27:16 +0400

    lesson21

--- Sm2.py
+++ Sm2.py
@@ -6,13 +6,69 @@
 #         print(x.index(y))
     
 
-x = [-1, 4, 5, 8, -10, 20]
-y =int(input('введите число '))
-z =[]
-for i in x:
-    if abs(i - y) == 0:
-        print(x.index(i))
-    elif abs(i - y) > abs((i + 1) - y):
-        c = i
-print(c)
+# x = [-1, 4, 5, 8, -10, 20]
+# y =int(input('введите число '))
+# z =[]
+# for i in x:
+#     if abs(i - y) == 0:
+#         print(x.index(i))
+#     elif abs(i - y) > abs((i + 1) - y):
+#         c = i
+# print(c)
         
+
+# calc 
+#  միայն աշխատում է դրական միանիշ թվերի դեպքում
+# def gum(x, y):
+#     return x + y
+
+# def han(x, y):
+#     return x - y
+
+# def baz(x, y):
+#     return x * y
+
+# def baj(x, y):
+#     return x / y
+
+# s = input()
+# for i in range(0, len(s)+1):
+#     a = int(s[i])
+#     b = s[i+1]
+#     c = int(s[i+2])
+#     if b == '+':
+#         print(gum(x = a, y = c))
+#     elif b == '-':
+#         print(han(x = a, y = c))
+#     elif b == '*':
+#         print(baz(x = a, y = c))
+#     elif b == '/':
+#         print(gum(x = a, y = c))
+#     else:
+#         print('error')
+
+
+
+
+
+# Write a python program to find max of two numbers.
+# def func(x, y):
+#     if x > y:
+#         return f'{x} is max'
+#     else:
+#         return f'{y} is max'
+
+# print(func(5, 6))
+
+
+# Write a python program to sum all numbers
+# def func(x, y):
+#     return x + y
+# print(func(7, 8))
+
+
+
+# Write a python program to multiply all numbers
+def mult(x, y):
+    return x * y
+print(mult(7 , 8))
