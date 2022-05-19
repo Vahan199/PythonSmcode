c7e42eb7fe4062024899d6a1274e19751c726e5e
Vahan199 <Vahanxxx1@icloud.com>
2022-05-19 21:31:48 +0400

    sev

--- Sm2.py
+++ Sm2.py
@@ -107,25 +107,47 @@
 #             if st1[i+3].isdigit() and st1[i+4].isdigit() and st1[i+5].isdigit():
 #                 return 'ok'
 
-st1 = []
+# st1 = []
+# import random as r
+# def func(st1):
+#     a = ('a', 'b' , 'c', 'd')
+#     while True:
+#         a = ('a', 'b' , 'c', 'd')
+#         st1 = []
+#         st1 = st1.append(r.choise(a))
+#         if len(st1) < 6:
+#             print(len(st1))
+#         else:
+#             for i in range(len(st1)+1):
+#                 if st1[i].isalpha() and st1[i+1].isalpha() and st1[i+2].isalpha():
+#                     return 'ok'
+#                 if st1[i+3].isdigit() and st1[i+4].isdigit() and st1[i+5].isdigit():
+#                     return 'ok'
+# print(func(st1 = st1.append(r.a)))
+
+
+
+
+
+
 import random as r
-def func(st1):
-    a = ('a', 'b' , 'c', 'd')
-    while True:
-        a = ('a', 'b' , 'c', 'd')
-        st1 = []
-        st1 = st1.append(r.choise(a))
-        if len(st1) < 6:
-            print(len(st1))
-        else:
-            for i in range(len(st1)+1):
-                if st1[i].isalpha() and st1[i+1].isalpha() and st1[i+2].isalpha():
-                    return 'ok'
-                if st1[i+3].isdigit() and st1[i+4].isdigit() and st1[i+5].isdigit():
-                    return 'ok'
-print(func(st1 = st1.append(r.a)))
+while True:
+    c = 0
+    q =0
+    n = []
+    st1 = []
+    st1.append(r.choice('qwertyuioasdfghjzxcvbnm'))
+    n.append(r.randint(0, 9))
+    for i in n:
+        if 'i'.isdigit():
+            c += 1
+        for j in st1:
+            if 'j'.isalpha():
+                q += 1
+    if q == 3 and c == 3:
+        print(n+st1)
+        break
 
-
-
+    
 
 
